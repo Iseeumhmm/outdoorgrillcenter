66262a2350d633e5ee718206f7fc1eff8a0bad5f
Rick Heffren <rickheffren@Ricks-MacBook-Pro-2.local>
2025-11-10 08:30:54 -0500

    Switcher is working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
+import { Providers } from "@/components/providers";
 
 export const metadata: Metadata = {
   title: "Outdoor Grill Center",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
